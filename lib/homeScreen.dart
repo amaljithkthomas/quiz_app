5dd7a5ede59d294ff34fc5b007be3e30f850cc05
AMALJITH THOMAS <amaljithkthomas@gmail.com>
2022-07-13 23:24:58 +0530

    QUIZ APP STATED

---
+++ lib/homeScreen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Quiz(),
+        ),
+      ),
+    );
+  }
+}
+
+class Quiz extends StatefulWidget {
+  const Quiz({Key? key}) : super(key: key);
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: Text(
+              'This is where the question text will go',
+              style: TextStyle(color: Colors.white, fontSize: 17),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+                side: MaterialStateProperty.all(
+                  BorderSide(
+                    style: BorderStyle.solid,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+                side: MaterialStateProperty.all(
+                  BorderSide(
+                    style: BorderStyle.solid,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Icon(Icons.check,color: Colors.green,),
+              Icon(Icons.close,color: Colors.red,),
+
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
